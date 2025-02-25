821321883e38d456aa4046fee2e967e06ff3c028
Manuel Cruz Sánchez <a>
2025-02-25 14:26:44 +0000

    creado repositorio git

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Footer from "../../components/footer/Footer"
+import Header from "../../components/header/Header"
+
+
+function Home() {
+
+  return (
+    <>
+      <Header />
+      <p>pagina HOME</p>
+      <p>asdasd</p>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
